d264df655e26db0e3be3ea2645ddb978bf404550
Hekman <yayakinetic@gmail.com>
2024-09-02 23:51:03 +0800

    chart and main header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import ChartComponent from "@/components/Chart/ChartComponent";
+import HeaderComponent from "@/components/Main/HeaderComponent";
 export default function Home() {
-  return <main className="bg-neutral-100">Hello</main>;
+  return (
+    <main className=" px-4 pt-10">
+      <HeaderComponent />
+      <div className="bg-neutral-50 w-full">
+        <ChartComponent />
+      </div>
+    </main>
+  );
 }
---
+++ components/Main/HeaderComponent.tsx
@@ -0,0 +1,27 @@
+import { Button } from "../ui/button";
+
+export default function HeaderComponent() {
+  return (
+    <header className="pb-5 flex justify-between items-center">
+      <div className="flex flex-col gap-1">
+        {/* title */}
+        <h1 className="text-xl font-bold tracking-wider text-neutral-600">
+          Order Management System
+        </h1>
+        <p className="text-sm font-bold text-neutral-400">
+          A dashboard make your life easier.
+        </p>
+      </div>
+
+      {/* function button */}
+      <div className="flex items-center justify-center gap-4">
+        <Button variant={"default"} className="font-bold">
+          Add new order
+        </Button>
+        <Button variant={"secondary"} className="font-bold">
+          search order
+        </Button>
+      </div>
+    </header>
+  );
+}
